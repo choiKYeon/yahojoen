ee6e78bbc174871563b7ef1085b3fc184980d238
ckh <gusrudchl12@gmail.com>
2023-11-01 11:56:12 +0900

    11-01yahoproj

--- src/main/java/org/example/review/controller/ReviewController.java
+++ src/main/java/org/example/review/controller/ReviewController.java
@@ -29,7 +29,6 @@ public class ReviewController {
 
         mainScreen.mainSelect();
     }
-
     public void fullReview() {
         System.out.println("리뷰 번호 / 제목 / 내용 / 작성자");
         System.out.println("-".repeat(29));
@@ -39,7 +38,6 @@ public class ReviewController {
         System.out.println("리뷰 목록이 출력되었습니다.");
         mainScreen.mainSelect();
     }
-
     public void myReview() {
 
         if (reviewRepository.getReviews().size() == 0) {
@@ -56,11 +54,9 @@ public class ReviewController {
                 return;
             }
         }
-
         System.out.println("나의 리뷰 목록이 출력되었습니다.");
         mainScreen.mainSelect();
     }
-
     public void modify() {
 
         if (reviewRepository.getReviews().size() == 0) {
@@ -68,13 +64,12 @@ public class ReviewController {
             mainScreen.mainSelect();
             return;
         }
-
         reviewService.myReviewListService();
 
         for (int i = 0; i < reviewRepository.getReviews().size(); i++) {
             if (ReviewRepository.getReviews().get(i).getUserId().equals(Container.getCheckedmembers().getUserId()) == false) {
                 System.out.println("작성한 리뷰가 없습니다.");
-            }else {
+            } else {
                 System.out.println("나의 리뷰 목록이 출력되었습니다. 수정할 리뷰번호를 선택해주세요.");
                 System.out.print("입력 :");
                 int id = Container.getSc().nextInt();
@@ -102,10 +97,8 @@ public class ReviewController {
                 System.out.println(id + "번 리뷰가 수정되었습니다.");
             }
         }
-
-            mainScreen.mainSelect();
-        }
-
+        mainScreen.mainSelect();
+    }
     public void remove() {
 
         if (reviewRepository.getReviews().size() == 0) {
@@ -120,7 +113,7 @@ public class ReviewController {
             if (ReviewRepository.getReviews().get(i).getUserId().equals(Container.getCheckedmembers().getUserId()) == false) {
                 System.out.println("작성한 리뷰가 없습니다.");
 
-            }else {
+            } else {
                 System.out.println("나의 리뷰 목록이 출력되었습니다. 삭제할 리뷰번호를 선택해주세요.");
                 System.out.print("입력 :");
                 int id = Container.getSc().nextInt();
@@ -137,7 +130,6 @@ public class ReviewController {
                 System.out.println(id + "번 리뷰가 삭제되었습니다.");
 
                 Container.getSc().nextLine().trim();
-
             }
         }
         mainScreen.mainSelect();
